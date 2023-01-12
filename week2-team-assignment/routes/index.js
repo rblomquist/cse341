b8cd4c5106740c9eb4295a021e059eb2c2f3a679
rblomquist <103947874+rblomquist@users.noreply.github.com>
2023-01-12 17:01:31 -0500

    Delete week2-team-assignment directory

--- week2-team-assignment/routes/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-const professional = require("./professional")
-
-router.use('/professional', professional);
-
-module.exports = router;
--- week2-team-assignment/routes/professional.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const model = require("../modules/professional");
-
-router.get("/:id", model.getProfessional);
-
-module.exports = router;
