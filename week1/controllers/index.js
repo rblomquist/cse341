75175ba96034f872e5b780579e04b0723938c247
rblomquist <103947874+rblomquist@users.noreply.github.com>
2023-01-12 17:01:11 -0500

    Delete week1 directory

--- week1/controllers/index.js
+++
@@ -1,9 +0,0 @@
-displayJoke = (req, res) => {
-    const data =
-      'Why did the lollipop cross the road? ... It was stuck to the chicken!';
-    res.status(200).send(data);
-  };
-  
-  module.exports = {
-    displayJoke,
-  };
