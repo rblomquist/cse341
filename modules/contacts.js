d10d29491df02ae3590b392fa36dc0fefdfa91cf
rblomquist <rblomquist@gmail.com>
2023-02-08 13:24:31 -0500

    cleanup

--- modules/contacts.js
+++ modules/contacts.js
@@ -1,33 +1,39 @@
 const mongodb = require("../db/connect");
 const ObjectId = require('mongodb').ObjectId;
 
-const getAll = async (req, res) => {
-  const result = await mongodb
+const getAll = (req, res) => {
+  mongodb
     .getDb()
     .db()
     .collection('contacts')
-    .find();
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists);
-  });
+    .find()
+    .toArray((err, lists) => {
+      if (err) {
+        res.status(400).json(err);
+      }
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(lists);
+    });
 };
 
-const getSingle = async (req, res) => {
+const getSingle = (req, res) => {
   if (!ObjectId.isValid(req.params.id)) {
     res.status(400).json("Must use valid contact ID to find a contact")
   };
   const userId = new ObjectId(req.params.id);
-  const result = await mongodb
+  mongodb
     .getDb()
     .db()
     .collection('contacts')
-    .find({ _id: userId });
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
-  });
-};  
+    .find({ _id: userId })
+    .toArray((err, lists) => {
+      if (err) {
+        res.status(400).json(err);
+      }
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(lists);
+    });
+};
 
 const addContact = async (req, res) => {
   const contact = {
